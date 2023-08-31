b9dd401a556afa83f61c33731d0cafe0fb405f3c
Misaka-L <lipww1234@foxmail.com>
2023-09-01 01:02:58 +0800

    chore: finally work

--- Packages/com.vrcau.security-check/Runtime/Scripts/SecurityDoor.cs
+++ Packages/com.vrcau.security-check/Runtime/Scripts/SecurityDoor.cs
@@ -1,33 +1,49 @@
-﻿using UdonSharp;
+﻿using System;
+using UdonSharp;
 using UnityEngine;
+using VRC.SDKBase;
 using VRC.Udon.Common.Enums;
+using VRC.Udon.Common.Interfaces;
 
-[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
-[RequireComponent(typeof(Collider))]
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
 public class SecurityDoor : UdonSharpBehaviour
 {
     private AudioSource _audioSource;
 
+    public GameObject testObject;
+    public float rayDistance = 5f;
+    public LayerMask layerMask = -1;
+
     private void Start()
     {
         _audioSource = GetComponent<AudioSource>();
+        testObject.SetActive(false);
     }
 
-    public void _Reset()
+    public void Test()
     {
+        testObject.SetActive(true);
+    }
+
+    public void _TestNetworked() => SendCustomNetworkEvent(NetworkEventTarget.All, nameof(Test));
+
+    public void Reset()
+    {
+        testObject.SetActive(false);
         _audioSource.Stop();
     }
+
+    public void _RestNetworked() => SendCustomNetworkEvent(NetworkEventTarget.All, nameof(Reset));
 
     public void _Play()
     {
         _audioSource.Play();
     }
 
-    private void OnTriggerEnter(Collider other) => _Play();
-
-    private void OnTriggerExit(Collider other) => _Reset();
-
-    private void OnCollisionEnter(Collision other) => _Play();
-
-    private void OnCollisionExit(Collision other) => _Reset();
+    private void FixedUpdate()
+    {
+        if (Physics.Raycast(transform.position + Vector3.down, Vector3.up, rayDistance,
+                layerMask) && !_audioSource.isPlaying)
+            _Play();
+    }
 }
